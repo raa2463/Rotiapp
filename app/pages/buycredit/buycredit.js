7408d62cea6cce02f2de1ba05a23421baf0250a9
Ali <raa2463@gmail.com>
2017-03-19 12:55:40 +0100

    Another Page added
    Another Page added

---
+++ app/pages/buycredit/buycredit.js
@@ -0,0 +1,7 @@
+var BasePage = require("../../shared/BasePage");
+
+var BuyCreditPage = function() {};
+BuyCreditPage.prototype = new BasePage();
+BuyCreditPage.prototype.constructor = BuyCreditPage;
+
+module.exports = new BuyCreditPage();
